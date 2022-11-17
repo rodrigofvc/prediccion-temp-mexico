9b06db7ad38cd0d633aae4e6a550ab60ca9ca762
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2022-11-17 15:16:44 -0600

    actualizacion de regresión

--- regresion/RegresionLineal.py
+++ regresion/RegresionLineal.py
@@ -6,6 +6,8 @@ from sklearn.kernel_ridge import KernelRidge
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.dates as mdates
+import pandas as pd
 
 
 class RegresionLineal(object):
@@ -14,36 +16,49 @@ class RegresionLineal(object):
         self.X = X
         self.Y = Y
 
-    def regresion_lineal(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, random_state=0)
+    def regresion_lineal(self, seed, show):
+        hora_local = self.Y.columns[1];
+        fechas = self.Y.get([hora_local])
 
+        temperatura = self.Y.columns[0];
+        self.Y = self.Y.get([temperatura])
+
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, random_state=seed)
+
+        """
         print(">>>> Usando regresion lineal")
         linreg= LinearRegression().fit(X_train, y_train)
         print("Interseccion (b): {}".format(linreg.intercept_))
         print("Pesos (w): {}".format(linreg.coef_))
         r1_score = linreg.score(X_test, y_test)
         print("Score - RegresionLineal: {}".format(r1_score))
+        """
+
 
         # Normalizacion
         scaler = MinMaxScaler()
         scaler.fit(X_train)
         X_train_scaled= scaler.transform(X_train)
         X_test_scaled= scaler.transform(X_test)
+        X_scaled = scaler.transform(self.X)
 
+        """
         print(">>>> Usando Ridge")
-        clf = Ridge(alpha=1.0).fit(X_train_scaled, y_train)
+        clf = Ridge(alpha=0.1).fit(X_train_scaled, y_train)
         print("Interseccion (b): {}".format(clf.intercept_))
         print("Pesos (w): {}".format(clf.coef_))
         r2_score = clf.score(X_test_scaled, y_test)
         print("Score - Ridge: {}".format(r2_score))
+        """
 
-        print(">>>> Usando Kernel Ridge")
+        #print(">>>> Usando Kernel Ridge")
         # Polinomio de grado 6
-        krr = KernelRidge(alpha=1.0, kernel='polynomial', degree=6)
+        krr = KernelRidge(alpha=0.1, kernel='polynomial', degree=6)
         krr.fit(X_train_scaled, y_train)
         r3_score = krr.score(X_test_scaled, y_test)
-        print("Score - Ridge Kernel: {}".format(r3_score))
+        #print("Score - Ridge Kernel: {}".format(r3_score))
 
+        """
         fig, axs = plt.subplots(2, 2)
         # Regresion lineal
         axs[0,0].plot(y_test.values, '-o', label='test')
@@ -60,5 +75,18 @@ class RegresionLineal(object):
         axs[1,0].plot(krr.predict(X_test_scaled), '-o', label='prediccion')
         axs[1,0].legend(loc="upper left")
         axs[1,0].set_title('Regresion Kernel Ridge  score:{:.6f}'.format(r3_score))
+        """
 
-        plt.show()
+        plt.plot(fechas.to_numpy().reshape(len (fechas)), self.Y.values, '-o', label='y_test')
+        plt.plot(fechas.to_numpy().reshape(len (fechas)), krr.predict(X_scaled), '-o', label='y_pred')
+        plt.xlabel("Fecha (YYYY-MM-DD)")
+        plt.ylabel("Temperatura (°C)")
+        plt.grid()
+        plt.gca().tick_params(axis='x', labelrotation=45)
+        plt.legend(loc="upper left")
+        plt.title('Prediccion temperatura 9 a.m. (dias a futuro)')
+        print(r3_score)
+        if show:
+            plt.show()
+
+        return r3_score
