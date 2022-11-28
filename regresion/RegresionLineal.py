b38eac6bc433095ee4213b78456cf552b377a98f
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2022-11-28 09:59:43 -0600

    funciones para graficar todo el conjunto

--- regresion/RegresionLineal.py
+++ regresion/RegresionLineal.py
@@ -16,24 +16,14 @@ class RegresionLineal(object):
         self.X = X
         self.Y = Y
 
-    def regresion_lineal(self, seed):
+    def regresion_lineal(self, seed=99):
         hora_local = self.Y.columns[1];
         fechas = self.Y.get([hora_local])
 
         temperatura = self.Y.columns[0];
         self.Y = self.Y.get([temperatura])
 
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, random_state=seed)
-
-        """
-        print(">>>> Usando regresion lineal")
-        linreg= LinearRegression().fit(X_train, y_train)
-        print("Interseccion (b): {}".format(linreg.intercept_))
-        print("Pesos (w): {}".format(linreg.coef_))
-        r1_score = linreg.score(X_test, y_test)
-        print("Score - RegresionLineal: {}".format(r1_score))
-        """
-
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, random_state=99)
 
         # Normalizacion
         scaler = MinMaxScaler()
@@ -42,49 +32,57 @@ class RegresionLineal(object):
         X_test_scaled= scaler.transform(X_test)
         X_scaled = scaler.transform(self.X)
 
-        print(">>>> Usando Ridge")
+        #print(">>>> Usando Ridge")
         clf = Ridge(alpha=0.1).fit(X_train_scaled, y_train)
-        print("Interseccion (b): {}".format(clf.intercept_))
-        print("Pesos (w): {}".format(clf.coef_))
+        #print("Interseccion (b): {}".format(clf.intercept_))
+        #print("Pesos (w): {}".format(clf.coef_))
         r2_score = clf.score(X_test_scaled, y_test)
-        print("Score - Ridge: {}".format(r2_score))
+        #print("Score - Ridge: {}".format(r2_score))
 
-        print(">>>> Usando Kernel Ridge")
+        #print(">>>> Usando Kernel Ridge")
         # Polinomio de grado 6
-        krr = KernelRidge(alpha=0.1, kernel='polynomial', degree=4)
+        krr = KernelRidge(alpha=0.1, kernel='polynomial', degree=3)
         krr.fit(X_train_scaled, y_train)
         r3_score = krr.score(X_test_scaled, y_test)
-        print("Score - Ridge Kernel: {}".format(r3_score))
+        #print("Score - Ridge Kernel: {}".format(r3_score))
 
-        """
-        fig, axs = plt.subplots(2, 2)
-        # Regresion lineal
-        axs[0,0].plot(y_test.values, '-o', label='test')
-        axs[0,0].plot(linreg.predict(X_test), '-o', label='prediccion')
-        axs[0,0].legend(loc="upper left")
-        axs[0,0].set_title('Regresion lineal simple   score:{:.6f}'.format(r1_score))
-        # Regresion Ridge
-        axs[0,1].plot(y_test.values, '-o', label='test')
-        axs[0,1].plot(clf.predict(X_test_scaled), '-o', label='prediccion')
-        axs[0,1].legend(loc="upper left")
-        axs[0,1].set_title('Regresion Ridge  score:{:.6f}'.format(r2_score))
-        # Regresion Kernel Ridge con polinomio 6
-        axs[1,0].plot(y_test.values, '-o', label='test')
-        axs[1,0].plot(krr.predict(X_test_scaled), '-o', label='prediccion')
-        axs[1,0].legend(loc="upper left")
-        axs[1,0].set_title('Regresion Kernel Ridge  score:{:.6f}'.format(r3_score))
-        """
 
-        """
-        plt.plot(fechas.to_numpy().reshape(len (fechas)), self.Y.values, '-o', label='y_test')
-        plt.plot(fechas.to_numpy().reshape(len (fechas)), krr.predict(X_scaled), '-o', label='y_pred')
-        plt.xlabel("Fecha (YYYY-MM-DD)")
-        plt.ylabel("Temperatura (°C)")
-        plt.grid()
-        plt.gca().tick_params(axis='x', labelrotation=45)
-        plt.legend(loc="upper left")
-        plt.title('Prediccion temperatura 9 a.m. (dias a futuro)')
-        """
+        max_score =  max(r2_score, r3_score)
+        Y_pred = krr.predict(X_test_scaled)
+        Y_real = y_test
+        fechas = fechas.to_numpy().reshape(len (fechas))
+
+        return max_score, Y_pred, Y_real, fechas
+
+    # Devuelve las predicciones del conjunto X y las reales de todo el conjunto
+    def regresion_lineal_total(self, seed=99):
+        hora_local = self.Y.columns[1];
+        fechas = self.Y.get([hora_local])
+
+        temperatura = self.Y.columns[0];
+        self.Y = self.Y.get([temperatura])
+
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, random_state=99)
+
+        # Normalizacion
+        scaler = MinMaxScaler()
+        scaler.fit(X_train)
+        X_train_scaled= scaler.transform(X_train)
+        X_test_scaled= scaler.transform(X_test)
+        X_scaled = scaler.transform(self.X)
+
+        #print(">>>> Usando Ridge")
+        clf = Ridge(alpha=0.1).fit(X_train_scaled, y_train)
+        r2_score = clf.score(X_test_scaled, y_test)
+        #print("Score - Ridge: {}".format(r2_score))
+
+        #print(">>>> Usando Kernel Ridge")
+        # Polinomio de grado 6
+        krr = KernelRidge(alpha=0.1, kernel='polynomial', degree=3)
+        krr.fit(X_train_scaled, y_train)
+        r3_score = krr.score(X_test_scaled, y_test)
+        #print("Score - Ridge Kernel: {}".format(r3_score))
+
         max_score =  max(r2_score, r3_score)
 
         Y_pred = krr.predict(X_scaled)
--- utilidades/Graficador.py
+++ utilidades/Graficador.py
@@ -1,17 +1,66 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.pyplot import figure
 import matplotlib.dates as mdates
 import pandas as pd
 
 class Graficador(object):
 
-    def grafica(self, hora, dias_pasado, dias_futuro, score, Y_pred, Y_real, fechas):
-        plt.plot(fechas, Y_real, '-o', label='y_test')
+    def grafica_total(self, fechas, Y_real):
+        fechas = Y_real.get(['Hora Local'])
+        temperatura = Y_real.columns[0];
+        temps = Y_real.get([temperatura])
+        #fechas = fechas.to_numpy().reshape(len (fechas))
+        figure(figsize=(15, 8))
+        print()
+        print (fechas.values)
+        plt.plot(fechas.values, temps.values, '-o', label='temperatura')
+        plt.xlabel("Fecha (YYYY-MM-DD)")
+        plt.ylabel("Temperatura (°C)")
+        plt.grid()
+        plt.gca().tick_params(axis='x')
+        #plt.gca().tick_params(axis='x', labelrotation=45)
+        plt.legend(loc="upper left")
+        plt.title('Temperatura a las 9 AM desde el 17 de Julio hasta el 11 de Noviembre')
+        plt.savefig('total-17-29')
+        plt.show()
+
+    # Grafica todo el vector X     
+    def grafica_fechas(self, fechas, Y_real, Y_pred, hora, fecha_cercana, fecha_lejana):
+        #fechas = fechas.to_numpy().reshape(len (fechas))
+        figure(figsize=(15, 8))
+        plt.plot(fechas, Y_real, '-o', label='y_real')
         plt.plot(fechas, Y_pred, '-o', label='y_pred')
         plt.xlabel("Fecha (YYYY-MM-DD)")
         plt.ylabel("Temperatura (°C)")
         plt.grid()
-        plt.gca().tick_params(axis='x', labelrotation=45)
+        plt.gca().tick_params(axis='x')
+        #plt.gca().tick_params(axis='x', labelrotation=45)
         plt.legend(loc="upper left")
-        plt.title('Predicción temperatura {} ( {} dias a futuro, usando {} dias al pasado ) '.format(hora, dias_futuro, dias_pasado))
+        plt.title('Temperatura a las {} desde {} hasta {}'.format(hora, fecha_lejana, fecha_cercana))
+        plt.show()
+
+
+    def grafica(self, hora, dias_pasado, dias_futuro, Y_pred, Y_real, mse, rmse, mae, horaEtq, sin_var):
+        figure(figsize=(15, 8))
+
+        X = np.arange(1,len(Y_pred)+1,1)
+        plt.scatter(X, Y_real.values, label='y_true', color="g")
+        plt.scatter(X, Y_pred, label='y_pred', color="blue")
+        plt.xlabel("Instancia")
+        plt.ylabel("Temperatura (°C)")
+        plt.xticks(X)
+        plt.grid()
+        plt.gca().tick_params(axis='x')
+        #plt.gca().tick_params(axis='x', labelrotation=45)
+        plt.legend(loc="upper left")
+        plt.title('Predicción temperatura {} (info. de {} dias al pasado, pred. de {} dias a futuro) MSE: {:.2f} RMSE: {:.2f} MAE: {:.2f} '.format(hora, dias_pasado, dias_futuro, mse, rmse, mae))
+        if sin_var:
+            dir_var = 'img/no_var/'
+        else:
+            dir_var = 'img/var/'
+        dir = dir_var + horaEtq[0] + horaEtq[1] + '/'
+        nombre = horaEtq[0] + horaEtq[1] + "-" + str(dias_pasado) + "ps-" + str(dias_futuro) + "ft"
+        print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DIR {}'.format(dir+nombre))
+        #plt.savefig(dir + nombre)
         plt.show()
